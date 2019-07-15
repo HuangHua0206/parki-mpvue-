5aaa7b440f593cf7c189688bdce463d2dff866c3
xiaoxinqing <1548920256@qq.com>
2019-07-16 00:58:24 +0800

    提交一下

--- src/pages/collect/index.vue
+++ src/pages/collect/index.vue
@@ -54,25 +54,26 @@
 			</div>
 			<div class="plus"  @click="plus"></div>
 		</div>
+		<!-- 能量背包-->
 		<div   class="energy-wrap" :class="{fadeUp: energyIn}">
-			<div class="boll ">
+			<div class="boll " @click="getBagEnergy('yellow')">
 				<div class="round yellow"></div>
-				<div class="num">99</div>
+				<div class="num">{{bags.yellow}}</div>
 				<div class="little-round"></div>
 			</div>
-			<div class="boll ">
+			<div class="boll " @click="getBagEnergy('blue')">
 				<div class="round blue"></div>
-				<div class="num">99</div>
+				<div class="num">{{bags.blue}}</div>
 				<div class="little-round"></div>
 			</div>
-			<div class="boll ">
+			<div class="boll " @click="getBagEnergy('green')">
 				<div class="round green"></div>
-				<div class="num">99</div>
+				<div class="num">{{bags.green}}</div>
 				<div class="little-round"></div>
 			</div>
-			<div class="boll ">
+			<div class="boll " @click="getBagEnergy('orange')">
 				<div class="round orange"></div>
-				<div class="num">99</div>
+				<div class="num">{{bags.orange}}</div>
 				<div class="little-round"></div>
 			</div>
 		</div>
@@ -90,6 +91,9 @@
 		<div class="pop-up-right" :class="{fadeUp: which === 'bracelet'}">
 			<Bracelet   @closePop="close"   />
 		</div>
+	<!-- 	<div class="pop-up-right" :class="{fadeUp: which === 'animal'}">
+			<Animal   @closePop="close"   />
+		</div> -->
 		<div 
 			class="collect-sunny"
 	 		v-if="video.play"
@@ -107,12 +111,52 @@
 			/>
 			
 		</div>
-		<div class="energy" :class="{
-			'blue': video.energyType === 'blue' || video.energyType === 'blue-finish',
-			'yellow': video.energyType === 'yellow' || video.energyType === 'yellow-finish',
-			'green': video.energyType === 'green' || video.energyType === 'green-finish',
-			'orange': video.energyType === 'orange' | video.energyType === 'orange-finish',
-			'show': true
+		<!-- 蓝牙获取能量（包含动画效果） -->
+		<div class="energy energy-born" :class="{
+			'blue': collects[0] === 102 ,
+			'yellow': collects[0] === 101,
+			'green': collects[0] === 103,
+			'orange': collects[0] === 100,
+			'show': video.energyShow1,
+			'down1': video.energyDown1 
+		}"></div>
+		<div class="energy energy-born" :class="{
+			'blue': collects[1] === 102,
+			'yellow': collects[1] === 101,
+			'green': collects[1] === 103,
+			'orange': collects[1] === 100,
+			'show': video.energyShow2,
+			'down2': video.energyDown2
+		}"></div>
+		<div class="energy energy-born" :class="{
+			'blue': collects[2] === 102 ,
+			'yellow': collects[2] === 101,
+			'green': collects[2] === 103,
+			'orange': collects[2] === 100,
+			'show': video.energyShow3,
+			'down3': video.energyDown3
+		}"></div>
+		<!-- 背包获取能量 -->
+		<div class="bag-energy address1" :class="{
+			'show': bags.bagShow1,
+			'blue': collects[0] === 102 ,
+			'yellow': collects[0] === 101,
+			'green': collects[0] === 103,
+			'orange': collects[0] === 100,
+		}"></div>
+		<div class="bag-energy address2" :class="{
+			'show': bags.bagShow2,
+			'blue': collects[1] === 102 ,
+			'yellow': collects[1] === 101,
+			'green': collects[1] === 103,
+			'orange': collects[1] === 100,
+		}"></div>
+		<div class="bag-energy address3" :class="{
+			'show': bags.bagShow3,
+			'blue': collects[2] === 102 ,
+			'yellow': collects[2] === 101,
+			'green': collects[2] === 103,
+			'orange': collects[2] === 100,
 		}"></div>
 	</div>
 	
@@ -123,19 +167,35 @@
 	import Task from './task'
 	import Range from './range'
 	import Bracelet from './bracelet'
+	import Animal from './animal'
 	export default{
 		data () {
 		  return {
 		  	fadeIn: false, // 界面加载完时四个弹窗按钮动画效果进入
 		  	video: { // 收集能量时视频相关参数
 		  		play: false, // 检测到能量时播放视频
+		  		address: 0, // 能量当前放置位置
 		  		energyType: '', // 能量类型（4种，blue，orange，yellow，green）
-		  		energyShow: false // 视频将播放完时显示生成的能力球
+		  		energyShow1: false, // 视频将播放完时显示生成的能力球
+		  		energyDown1: false,
+		  		energyShow2: false, // 视频将播放完时显示生成的能力球
+		  		energyDown2: false,
+		  		energyShow3: false, // 视频将播放完时显示生成的能力球
+		  		energyDown3: false
 		  	},
-		  	collects: [], // 当前收集能量集  	
+		  	bags: {
+		  		yellow: 0,
+		  		blue:2,
+		  		green:4,
+		  		orange:8,
+		  		bagShow1: false,
+		  		bagShow2: false,
+		  		bagShow3: false
+		  	},
+		  	collects: [null, null, null], // 当前收集能量集  	
 		  	online: false, // 是否绑定手环
 		  	blueStatus: false, // 蓝牙是否开启
-		  	which: '', // 界面显示某一个弹窗
+		  	which: 'animal', // 界面显示某一个弹窗
 		  	energyIn: false, // 能量背包是否显示
 		  	hasBracelet: true, //是否绑定手环
 		  	ISSAME: false, // 发现重复颜色能量
@@ -230,6 +290,7 @@
 		       console.log('发送能量请求');
 		       this.video.play = true;
 		       const major = beacons[0].major
+		       
 		       switch (major) {
 		       	  case 100:
 		       	    this.video.energyType = 'orange'
@@ -247,16 +308,65 @@
 		       setTimeout(() => {
 		       	this.video.play = false
 		       	this.video.energyType += '-finish'
-		      // 	this.video.energyShow = false
+		      	if (!this.collects[0]) {
+		      		this.collects[0] = major
+		      		this.video.energyDown1 = true
+		      	} else if (!!this.collects[0] && !this.collects[1]) {
+		      		this.collects[1] = major
+		      		this.video.energyDown2 = true
+		      	} else {
+		      		this.collects[2] = major
+		      		this.video.energyDown3 = true
+		      	}
 		       }, 3000);
 		        setTimeout(() => {
-		       	this.video.energyShow = true
+		        	 if (!this.collects[0]) {
+		        	 	console.log(0)
+			      		this.video.energyShow1 = true
+			      	} else if (!!this.collects[0] && !this.collects[1]) {
+			      		console.log(1)
+			      		this.video.energyShow2 = true
+			      	} else {
+			      		console.log(2)
+			      		this.video.energyShow3 = true
+			      	}
+	 
 		       }, 2300);
-		       this.collects.push(major)
-		       console.log(this.collects, 'collects')
+		      
+		       console.log(this.collects, 'collects', this.video)
 		       this.ISENDING = false
 
 		    },
+		    getBagEnergy(color) {
+		    	console.log(color)
+		    	let major = null
+		    	 switch (color) {
+		       	  case 'orange':
+		       	    major = 100
+		       	    break
+		       	  case 'yellow':
+		       	    major = 101
+		       	    break
+		       	  case 'blue':
+		       	    major = 102
+		       	    break
+		       	  case 'green':
+		       	    major = 103
+		       	    break
+		       }
+		       console.log(major)
+		    	if (!this.collects[0]) {
+		    		this.collects[0] = major
+		      		this.bags.bagShow1 = true
+		      	} else if (!!this.collects[0] && !this.collects[1]) {
+		      		this.collects[1] = major
+		      		this.bags.bagShow2 = true
+		      	} else {
+		      		this.collects[2] = major
+		      		this.bags.bagShow3 = true
+		      	}
+		      	console.log(this.collects, this.bags)
+		    }
 		},
 		onShow() {
 			this.openBlueTooth()
@@ -268,7 +378,7 @@
 		onUnload() {
 			this.fadeIn = false
 		},
-		components: { CommonTop, Email, Task, Range, Bracelet }
+		components: { CommonTop, Email, Task, Range, Bracelet, Animal }
 	}
 </script>
 <style lang="less">
@@ -486,7 +596,7 @@
 		position:absolute;
 		left:50%;
 		transform:translateX(-50%);
-		bottom:229px;
+		bottom:37%;
 		.mask{
 	 		// .bg("pl2_mask@2x");
 			position: absolute;
@@ -546,7 +656,7 @@
 	}
 	.nums-wrap{
 		position:absolute;
-		bottom:143px;
+		bottom:24%;
 		left:0;
 		width:100%;
 		height:65px;
@@ -708,12 +818,13 @@
 	.energy{
 		z-index:88;
 		position: absolute;
-		bottom: 84%;
+		bottom: 64%;
 		left: 50%;
-		height: 118px;
-		width:118px;
+		height: 20%;
+		width:30%;
 		transition: 0.5s;
 		opacity: 0;
+		.bg("pl2_ball_green@2x");
 		// display: none;
 		transform: translateX(-50%);
 		&.blue{
@@ -733,7 +844,63 @@
 			opacity: 1;
 			// display: block;
 		}
+		&.down1{
+			bottom: 27%;
+			width:12%;
+			height:7%;
+			left:26%;
+			z-index:7;
+		}
+		&.down2{
+			bottom: 27%;
+			width:12%;
+			height:7%;
+			z-index:7;
+		}
+		&.down3{
+			bottom: 27%;
+			width:12%;
+			height:7%;
+			left:74%;
+			z-index:7;
+		}
+		// &.enery-finish{
 
+		// }
 	} 
+	.bag-energy{
+		transition: 2s;
+		opacity: 0;
+		position: absolute;
+		height: 7%;
+		width:12%;
+		z-index:7;
+		bottom: 27%;
+		left:50%;
+		transform:translateX(-50%);
+		// .bg("pl2_ball_blue@2x");
+		&.blue{
+			.bg("pl2_ball_blue@2x");
+		}
+		&.green{
+			.bg("pl2_ball_green@2x");
+		}
+		&.yellow{
+			.bg("pl2_ball_yellow@2x");
+		}
+		&.orange{
+			.bg("pl2_ball_orange@2x");
+		}
+		&.show{
+			opacity: 1;
+
+		}
+		&.address1{
+			left: 26%
+		}
+		&.address3{
+			left:74%;
+		}
+	}
 }
 </style>
