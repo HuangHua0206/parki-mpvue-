8d03109d36554373733541ec4c7b89d6804f9533
huanghua <1548920256@qq.com>
2019-08-30 19:13:16 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App'
-import Toast from './utils/Toast'
-
+import Tips from './utils/tips'
+import store from "store";
 Vue.config.productionTip = false
 App.mpType = 'app'
-// Vue.prototype.$store = store
-Vue.prototype.$toast = Toast
-
+Vue.prototype.$tip = Tips
+Vue.prototype.$store = store;
 const app = new Vue(App)
-console.log(process.argv)
 app.$mount()
